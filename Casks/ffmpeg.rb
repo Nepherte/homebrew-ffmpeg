36caff92133e53451c199aff61a06bd5803fae6d
Bart Verhoeven <bartverhoeven@live.com>
2023-11-13 09:28:56 +0800

    ffmpeg 6.1 (#1)
    Update ffmpeg to 6.1.

--- Casks/ffmpeg.rb
+++ Casks/ffmpeg.rb
@@ -1,20 +1,20 @@
 cask "ffmpeg" do
+  version "6.1"
+  sha256 "a1d9289404b353619749d5d7108b8ded5c1be0d10d236ac13d2d4fc67f89b65b"
+
+  url "https://evermeet.cx/pub/ffmpeg/ffmpeg-#{version}.zip", verified: "evermeet.cx/"
   name "ffmpeg"
-  version "6.0.1"
-
+  desc "Complete, cross-patform solution to convert audio and video"
   homepage "https://ffmpeg.org/"
-  desc "A complete, cross-patform solution to convert audio and video"
-
-  url "https://evermeet.cx/pub/ffmpeg/ffmpeg-#{version}.zip", verified: "https://evermeet.cx"
-  sha256 "1b5bdb6be9f9601b6951672f99a18cb89c0d6e59ccc1453314f94b4d0579271b"
-
-  binary "ffmpeg"
-  depends_on macos: ">= :big_sur"
 
   livecheck do
     url "https://evermeet.cx/pub/ffmpeg/"
     regex(/href=.*?ffmpeg[._-](\d+\.\d+(?:\.\d+)?)\.zip/i)
   end
+
+  depends_on macos: ">= :big_sur"
+
+  binary "ffmpeg"
 
   caveats <<~EOS
     On macOS 10.15 (Catalina) and later, the binary has to be removed from quarantine:
